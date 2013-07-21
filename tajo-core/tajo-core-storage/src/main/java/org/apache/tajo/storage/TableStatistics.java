49f12fb8c642d8b91840a4979c1918f89194460a
se1 <sroesteves@gmail.com>
2013-07-21 01:42:58 +0100

    Added histogram and joinKeys vars.

--- tajo-core/tajo-core-storage/src/main/java/org/apache/tajo/storage/TableStatistics.java
+++ tajo-core/tajo-core-storage/src/main/java/org/apache/tajo/storage/TableStatistics.java
@@ -18,12 +18,13 @@
 
 package org.apache.tajo.storage;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.derby.iapi.types.DataType;
 import org.apache.tajo.catalog.Schema;
 import org.apache.tajo.catalog.statistics.ColumnStat;
 import org.apache.tajo.catalog.statistics.TableStat;
-import org.apache.tajo.common.TajoDataTypes;
-import org.apache.tajo.common.TajoDataTypes.DataType;
-import org.apache.tajo.common.TajoDataTypes.Type;
 import org.apache.tajo.datum.Datum;
 import org.apache.tajo.datum.NullDatum;
 
@@ -31,94 +32,98 @@ import org.apache.tajo.datum.NullDatum;
  * This class is not thread-safe.
  */
 public class TableStatistics {
-  private Schema schema;
-  private Tuple minValues;
-  private Tuple maxValues;
-  private long [] numNulls;
-  private long numRows = 0;
-  private long numBytes = 0;
+    private Schema schema;
+    private Tuple minValues;
+    private Tuple maxValues;
+    private long[] numNulls;
+    private long numRows = 0;
+    private long numBytes = 0;
 
+    private boolean[] comparable;
 
-  private boolean [] comparable;
+    private List<Integer> joinKeys;
 
-  public TableStatistics(Schema schema) {
-    this.schema = schema;
-    minValues = new VTuple(schema.getColumnNum());
-    maxValues = new VTuple(schema.getColumnNum());
-    /*for (int i = 0; i < schema.getColumnNum(); i++) {
-      minValues[i] = Long.MAX_VALUE;
-      maxValues[i] = Long.MIN_VALUE;
-    }*/
+    private Map<Tuple, Integer> histogram;
 
-    numNulls = new long[schema.getColumnNum()];
-    comparable = new boolean[schema.getColumnNum()];
+    public TableStatistics(Schema schema) {
+        this.schema = schema;
+        minValues = new VTuple(schema.getColumnNum());
+        maxValues = new VTuple(schema.getColumnNum());
+        /*
+         * for (int i = 0; i < schema.getColumnNum(); i++) { minValues[i] = Long.MAX_VALUE; maxValues[i] = Long.MIN_VALUE; }
+         */
 
-    DataType type;
-    for (int i = 0; i < schema.getColumnNum(); i++) {
-      type = schema.getColumn(i).getDataType();
-      if (type.getType() == Type.ARRAY) {
-        comparable[i] = false;
-      } else {
-        comparable[i] = true;
-      }
-    }
-  }
+        numNulls = new long[schema.getColumnNum()];
+        comparable = new boolean[schema.getColumnNum()];
 
-  public Schema getSchema() {
-    return this.schema;
-  }
-
-  public void incrementRow() {
-    numRows++;
-  }
-
-  public long getNumRows() {
-    return this.numRows;
-  }
-
-  public void setNumBytes(long bytes) {
-    this.numBytes = bytes;
-  }
-
-  public long getNumBytes() {
-    return this.numBytes;
-  }
-
-  public void analyzeField(int idx, Datum datum) {
-    if (datum instanceof NullDatum) {
-      numNulls[idx]++;
-      return;
-    }
-
-    if (datum.type() != TajoDataTypes.Type.ARRAY) {
-      if (comparable[idx]) {
-        if (!maxValues.contains(idx) ||
-            maxValues.get(idx).compareTo(datum) < 0) {
-          maxValues.put(idx, datum);
+        DataType type;
+        for (int i = 0; i < schema.getColumnNum(); i++) {
+            type = schema.getColumn(i).getDataType();
+            if (type.getType() == Type.ARRAY) {
+                comparable[i] = false;
+            } else {
+                comparable[i] = true;
+            }
         }
-        if (!minValues.contains(idx) ||
-            minValues.get(idx).compareTo(datum) > 0) {
-          minValues.put(idx, datum);
+    }
+
+    public Schema getSchema() {
+        return this.schema;
+    }
+
+    public void incrementRow() {
+        numRows++;
+    }
+
+    public long getNumRows() {
+        return this.numRows;
+    }
+
+    public void setNumBytes(long bytes) {
+        this.numBytes = bytes;
+    }
+
+    public long getNumBytes() {
+        return this.numBytes;
+    }
+
+    public void analyzeField(int idx, Datum datum) {
+        if (datum instanceof NullDatum) {
+            numNulls[idx]++;
+            return;
         }
-      }
-    }
-  }
 
-  public TableStat getTableStat() {
-    TableStat stat = new TableStat();
-
-    ColumnStat columnStat;
-    for (int i = 0; i < schema.getColumnNum(); i++) {
-      columnStat = new ColumnStat(schema.getColumn(i));
-      columnStat.setNumNulls(numNulls[i]);
-      columnStat.setMinValue(minValues.get(i));
-      columnStat.setMaxValue(maxValues.get(i));
-      stat.addColumnStat(columnStat);
+        if (datum.type() != TajoDataTypes.Type.ARRAY) {
+            if (comparable[idx]) {
+                if (!maxValues.contains(idx) || maxValues.get(idx).compareTo(datum) < 0) {
+                    maxValues.put(idx, datum);
+                }
+                if (!minValues.contains(idx) || minValues.get(idx).compareTo(datum) > 0) {
+                    minValues.put(idx, datum);
+                }
+            }
+        }
     }
 
-    stat.setNumRows(this.numRows);
-    stat.setNumBytes(this.numBytes);
+    public TableStat getTableStat() {
+        TableStat stat = new TableStat();
 
-    return stat;
-  }
+        ColumnStat columnStat;
+        for (int i = 0; i < schema.getColumnNum(); i++) {
+            columnStat = new ColumnStat(schema.getColumn(i));
+            columnStat.setNumNulls(numNulls[i]);
+            columnStat.setMinValue(minValues.get(i));
+            columnStat.setMaxValue(maxValues.get(i));
+            stat.addColumnStat(columnStat);
+        }
+
+        stat.setNumRows(this.numRows);
+        stat.setNumBytes(this.numBytes);
+
+        return stat;
+    }
+
+    public void setJoinKeys(List<Integer> joinKeys) {
+        this.joinKeys = joinKeys;
+    }
 }
